f5212911ac8703944e3525333f925a53c8317c90
Andy <alampers@umn.edu>
2016-07-24 21:06:53 -0500

    started

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="lemkelcp",
+    version='0.1',
+    author="Andy Lamperski",
+    author_email="alampers@umn.edu",
+    description=("A Python implementation of Lemke's Algorithm for linear complementarity problems"),
+    license="MIT",
+    install_requires=['numpy'],
+    keywords="linear complementarity problem lcp optimization",
+    packages=['lemkelcp'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Mathematics"
+    ]
+    
+)
